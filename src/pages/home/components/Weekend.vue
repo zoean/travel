842a90e21c27f5e70bcc2f5bc7c69cfa9fb681ae
zoean <webxly@gmail.com>
2019-04-10 14:17:03 +0800

    add recommend

---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,76 @@
+<template>
+  <div>
+    <div class="title">周末去哪儿</div>
+    <ul>
+      <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+        <div class="item-img-wrapper">
+          <img class="item-img" :src="item.imgUrl" alt="">
+        </div>
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeWeekend',
+  data () {
+    return {
+      recommendList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1603/6d/2f67ae0659f41f.jpg_r_640x214_bf6cbd0b.jpg',
+        title: '故宫',
+        desc: '世界五大宫之首，穿越与您近在咫尺'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1811/f3/86173f863bef61.jpg_r_640x214_52b003ac.jpg',
+        title: '八达岭长城',
+        desc: '世界五大宫之首，穿越与您近在咫尺'
+      }, {
+        id: '0003',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1811/7e/476589267ebb41.jpg_r_640x214_bf599709.jpg',
+        title: '故宫',
+        desc: '世界五大宫之首，穿越与您近在咫尺'
+      }, {
+        id: '0004',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1505/aa/7baaf8a851d221.jpg_r_640x214_1431200f.jpg',
+        title: '故宫',
+        desc: '世界五大宫之首，穿越与您近在咫尺'
+      }, {
+        id: '0005',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1505/9e/21df651e19af5d.jpg_r_640x214_3ea5bb38.jpg',
+        title: '故宫',
+        desc: '世界五大宫之首，穿越与您近在咫尺'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/mixins.styl'
+  .title
+    background: #eee
+    line-height: .8rem
+    text-indent: .2rem
+  .item-img-wrapper
+    height: 0
+    overflow: hidden
+    padding-bottom: 33.9%
+    .item-img
+      width: 100%
+  .item-info
+    padding: .1rem
+    .item-title
+      line-height: .54rem
+      font-size: .32rem
+      ellipsis()
+    .item-desc
+      line-height: .4rem
+      color: #ccc
+      ellipsis()
+</style>
