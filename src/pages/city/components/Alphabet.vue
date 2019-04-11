6d3ec03454b464d15c019b4dde39cee2e4d97659
zoean <webxly@gmail.com>
2019-04-11 13:43:39 +0800

    finish city css

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,30 @@
+<template>
+  <ul class="list">
+    <li class="item">A</li>
+    <li class="item">B</li>
+    <li class="item">C</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'CityAlphabet'
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl'
+.list
+  position: absolute
+  top: 1.58rem
+  right: 0
+  bottom: 0
+  width: .4rem
+  display: flex
+  flex-direction: column
+  justify-content: center
+  .item
+    text-align: center
+    line-height: .4rem
+    color: $bgColor
+</style>
