84f2dc5b7604b5b5e91ad36f5806559a061e5054
zoean <webxly@gmail.com>
2019-04-11 15:17:02 +0800

    ajax city

--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -21,48 +21,15 @@
     <div class="area">
       <div class="title border-topbottom">热门城市</div>
       <div class="button-list">
-        <div class="button-wrapper">
-          <div class="button">北京</div>
-        </div>
-        <div class="button-wrapper">
-          <div class="button">北京</div>
-        </div>
-        <div class="button-wrapper">
-          <div class="button">北京</div>
-        </div>
-        <div class="button-wrapper">
-          <div class="button">北京</div>
+        <div class="button-wrapper" v-for="item of hot" :key="item.id">
+          <div class="button">{{item.name}}</div>
         </div>
       </div>
     </div>
-    <div class="area">
-      <div class="title border-topbottom">A</div>
+    <div class="area" v-for="(item, key) of cities" :key="key">
+      <div class="title border-topbottom">{{key}}</div>
       <div class="item-list">
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-      </div>
-    </div>
-    <div class="area">
-      <div class="title border-topbottom">B</div>
-      <div class="item-list">
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-      </div>
-    </div>
-    <div class="area">
-      <div class="title border-topbottom">C</div>
-      <div class="item-list">
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom" v-for="innerItem of item" :key="innerItem.id">{{innerItem.name}}</div>
       </div>
     </div>
   </div>
@@ -73,6 +40,10 @@
 import Bscroll from 'better-scroll'
 export default {
   name: 'CityList',
+  props: {
+    hot: Array,
+    cities: Object
+  },
   mounted () {
     this.scroll = new Bscroll(this.$refs.wrapper)
   }
@@ -86,23 +57,6 @@ export default {
     border-color: #ccc
   &:after
     border-color: #ccc
-.title
-  line-height: .4rem
-  background: #eee
-  padding-left: .2rem
-  color: #666
-  font-size: .26rem
-.button-list
-  padding: .1rem
-  width: 33.33%
-  overfllow: hidden
-  .button-wrapper
-    float: left
-    width: 33.3%
-    .button
-      margin: .1rem
-      texe-align: center
-      border: .02rem solid #ccc
 .list
   position: absolute
   top: 1.58rem
