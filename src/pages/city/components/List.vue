85ca03a94b3ba6948bf731329125c8e7e179c7b5
zoean <webxly@gmail.com>
2019-04-11 11:09:27 +0800

    list commit

---
+++ src/pages/city/components/List.vue
@@ -0,0 +1,50 @@
+<template>
+<div class="list">
+  <div class="area">
+    <div class="title border-topbottom">当前城市</div>
+    <div class="button-list">
+      <div class="button-wrapper">
+        <div class="button">北京</div>
+      </div>
+    </div>
+  </div>
+  <div class="area">
+    <div class="title border-topbottom">热门城市</div>
+  </div>
+  <div class="area">
+    <div class="title border-topbottom">A</div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'CityList'
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl'
+.border-topbottom
+  &:before
+    border-color: #ccc
+  &:after
+    border-color: #ccc
+.title
+  line-height: .4rem
+  background: #eee
+  padding-left: .2rem
+  color: #666
+  font-size: .26rem
+.button-list
+  padding: .1rem
+  width: 33.33%
+  overfllow: hidden
+  .button-wrapper
+    float: left
+    width: 33.3%
+    .button
+      margin: .1rem
+      texe-align: center
+      border: .02rem solid #ccc
+</style>
